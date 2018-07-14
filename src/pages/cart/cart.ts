b41b17473117e9313072c768df179c3932bb5b00
simon humbert <humbertsimon@gmail.com>
2018-07-14 16:09:36 +0200

    Hotfix

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-=======
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
->>>>>>> Stashed changes
 import {RestaurantProvider} from "../../shared/providers/restaurant-provider";
 
 /**
