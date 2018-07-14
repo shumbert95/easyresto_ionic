2f3f3ff6d83db6d0ae244a244d828ef00b4fc875
simon humbert <humbertsimon@gmail.com>
2018-07-15 01:09:05 +0200

    Update reservation form

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {RestaurantProvider} from "../../shared/providers/restaurant-provider";
 
 /**
  * Generated class for the ReservationPage page.
@@ -15,12 +16,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReservationPage {
 
-  public myDate: any;
+  public myDate_day: any;
+  public myDate_hour: any;
   public cart: any;
-  public nbParticipants: number = 0;
+  public nbParticipants: any;
+  public restaurantSchedule: any;
+  public yearValues: any;
+  public hourValues = [];
+  public minDate: any;
+  public monthShortNames = ['Jan', 'Fév', 'Mars', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sept', 'Oct', 'Nov', 'Déc'];
+  public dayShortNames = ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private restaurantService: RestaurantProvider) {
       this.cart = this.getCart();
+      this.yearValues = [(new Date()).getFullYear(), new Date(new Date().setFullYear(new Date().getFullYear() + 1)).getFullYear()];
+      this.minDate = new Date().toISOString();
+      this.getSchedule();
   }
 
     getCart() {
@@ -30,10 +41,35 @@ export class ReservationPage {
     ionViewWillEnter() {
         this.cart = this.getCart();
     }
-    // onInputChange() {
-    //   if (!this.nbParticipants instanceof int ) {
-    //   }
-    // }
+    onNbParticipantsChange() {
+      this.nbParticipants <= 0 ? this.NbParticipants = 1 : '';
+    }
+
+  checkAvailability() {
+
+  }
+
+  onDateUpdate() {
+      let date = new Date(this.myDate_day);
+      let n = date.getDay();
+      for (let i = 0; i<this.restaurantSchedule[n].timeSteps.length; i++) {
+          let hour = this.restaurantSchedule[n].timeSteps[i].substr(0, 2);
+          if (this.hourValues.indexOf(hour) == -1) {
+            this.hourValues.push(hour);
+          }
+      }
+  }
+
+  getSchedule() {
+      this.restaurantService.getRestaurantSchedule(this.cart.restaurantId).then((result: any) => {
+          if (result) {
+              this.restaurantSchedule = result.result;
+          }
+      }, (err) => {
+          this.loading.dismiss();
+          this.presentToast(err);
+      });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReservationPage');
