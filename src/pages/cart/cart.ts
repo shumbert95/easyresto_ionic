1bb01665477311c2f1e80d22763048591a4cd009
simon humbert <humbertsimon@gmail.com>
2018-07-14 14:58:31 +0200

    Update inte detail restau + page cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,6 +1,12 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+<<<<<<< HEAD
 import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+=======
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+>>>>>>> Update inte detail restau + page cart
 import {RestaurantProvider} from "../../shared/providers/restaurant-provider";
+import {GoogleMaps, GoogleMap, GoogleMapsEvent, Marker} from "@ionic-native/google-maps";
+
 
 /**
  * Generated class for the CartPage page.
@@ -8,6 +14,15 @@ import {RestaurantProvider} from "../../shared/providers/restaurant-provider";
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
+declare let google;
+let map: any;
+let infowindow: any;
+let options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
 
 @IonicPage()
 @Component({
@@ -18,7 +33,11 @@ export class CartPage {
 
   cart: any;
   loading: any;
-  public restaurantData: any;
+    map: GoogleMap;
+    public latitude;
+    public longitude;
+    public markers = [];
+    public restaurantData: any;
 
     @ViewChild('map') mapElement: ElementRef;
   constructor(public navCtrl: NavController, public navParams: NavParams, private restaurantService: RestaurantProvider, private toastCtrl: ToastController) {
@@ -32,9 +51,39 @@ export class CartPage {
               this.restaurantData = result.result;
           }
       }, (err) => {
-          // this.loading.dismiss();
-          // this.presentToast(err);
+          this.loading.dismiss();
+          this.presentToast(err);
       });
+    }
+
+    initMap() {
+        navigator.geolocation.getCurrentPosition((location) => {
+            let userLat = String(location.coords.latitude);
+            let userLng = String(location.coords.longitude);
+            this.map = new google.maps.Map(this.mapElement.nativeElement, {
+                center: {lat: this.latitude, lng: this.longitude},
+                zoom: 15
+            });
+            let restaurantLoc = new google.maps.LatLng(parseFloat(this.latitude),parseFloat(this.longitude));
+            let marker = new google.maps.Marker({
+                map: this.map,
+                position: restaurantLoc,
+                title: "Restau !"
+            });
+
+            let userLoc = new google.maps.LatLng(parseFloat(userLat),parseFloat(userLng));
+
+            let userMarker = new google.maps.Marker({
+                map: this.map,
+                position: userLoc,
+                label: "M",
+                title: "Moi !"
+            });
+        }, (error) => {
+            console.log(error);
+        }, options);
+
+
     }
 
     getCart() {
